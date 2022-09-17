68d92fd47c9e27b535a04f612b6052300916ea3c
2 1 <korrektorrussia@mail.ru>
2022-09-17 16:32:17 +0700

    маша

---
+++ example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользоваеля:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+   Console.WriteLine("ura massha") ;
+}
+else
+{
+    Console.Write("hellow");
+    Console.WriteLine(username);
+}
